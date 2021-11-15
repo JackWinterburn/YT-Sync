8aa31037c1c59325a030f50be2f92c561ed7be1a
JackWinterburn <jack.winterburn4@hotmail.com>
2021-11-15 00:02:01 +0000

    got rid of unecessary react

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Static("/", "./public")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":8080"))
 }
